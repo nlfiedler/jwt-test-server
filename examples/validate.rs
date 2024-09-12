a488f667b90643876cacbeef5faa2d3e6746e3b7
Nathan Fiedler <nathanfiedler@fastmail.fm>
2024-09-11 20:31:38 -0700

    fix: replace hyper with reqwest for example
    Using the hyper crate for a simple HTTP client is very difficult, and when
    the API changes it comes a challenge to bring the example up to date.
    
    cargo test passes

--- examples/validate.rs
+++ examples/validate.rs
@@ -1,9 +1,6 @@
 //
-// Copyright (c) 2022 Nathan Fiedler
+// Copyright (c) 2024 Nathan Fiedler
 //
-use actix_web::web::Buf;
-use hyper::{Body, Client, Method, Request};
-use hyper_tls::HttpsConnector;
 use jsonwebtoken::{decode, jwk::JwkSet, Algorithm, DecodingKey, Validation};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -21,43 +18,37 @@ struct Claims {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-
     // retrieve an access token
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri("https://127.0.0.1:3000/tokens")
+    let client = reqwest::Client::new();
+    let url = reqwest::Url::parse("https://localhost:3000/tokens")?;
+    let res = client
+        .post(url.clone())
         .header("content-type", "application/x-www-form-urlencoded")
-        .body(Body::from(
-            r#"grant_type=password&username=johndoe&password=tiger2"#,
-        ))?;
-    let resp = client.request(req).await?;
-    if resp.status() != 200 {
-        let msg = "tokens request failed";
-        return Err(Box::<dyn std::error::Error + Send + Sync + 'static>::from(
-            msg,
-        ));
-    }
-    let body = hyper::body::to_bytes(resp.into_body()).await?;
-    let buf = body.reader();
-    let raw_value: Value = serde_json::from_reader(buf)?;
-    let token = raw_value.as_object().unwrap();
-    let access_token = token["access_token"].as_str().unwrap();
-
-    // retrieve the JSON web key set from the issuer
-    let uri = "https://127.0.0.1:3000/.well-known/jwks.json".parse()?;
-    let resp = client.get(uri).await?;
-    if resp.status() != 200 {
+        .body(r#"grant_type=password&username=johndoe&password=tiger2"#)
+        .send()
+        .await?;
+    if res.status() != 200 {
         let msg = "jwks request failed";
         return Err(Box::<dyn std::error::Error + Send + Sync + 'static>::from(
             msg,
         ));
     }
-    let body = hyper::body::to_bytes(resp.into_body()).await?;
-    let buf = body.reader();
-    let raw_data = std::io::read_to_string(buf)?;
-    let jwks: JwkSet = serde_json::from_str(&raw_data)?;
+    let raw_text = res.text().await?;
+    let raw_value: Value = serde_json::from_str(&raw_text)?;
+    let token = raw_value.as_object().unwrap();
+    let access_token = token["access_token"].as_str().unwrap();
+
+    // retrieve the JSON web key set from the issuer
+    let url = reqwest::Url::parse("https://localhost:3000/.well-known/jwks.json")?;
+    let res = client.get(url.clone()).send().await?;
+    if res.status() != 200 {
+        let msg = "jwks request failed";
+        return Err(Box::<dyn std::error::Error + Send + Sync + 'static>::from(
+            msg,
+        ));
+    }
+    let raw_text = res.text().await?;
+    let jwks: JwkSet = serde_json::from_str(&raw_text)?;
 
     // extract the first key from the set and validate the token
     let decoder = DecodingKey::from_jwk(&jwks.keys[0])?;
